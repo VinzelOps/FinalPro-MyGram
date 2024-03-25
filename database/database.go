4329d1c9304a915e6e6880976c08ea8b1aecd856
VinzelOps <88572751+VinzelOps@users.noreply.github.com>
2024-03-25 13:09:47 +0700

    adds

---
+++ database/database.go
@@ -0,0 +1,35 @@
+package database
+
+import (
+	"github.com/FinalPro-MyGram/models"
+	"fmt"
+	"log"
+	"os"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	db       *gorm.DB
+	err      error
+)
+
+func StartDB() {
+	godotenv.Load(".env")
+	db, err = gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("error connecting to database :", err)
+	}
+
+	fmt.Println("Successfully connected to database")
+	err = db.Debug().AutoMigrate(models.User{}, models.Photo{}, models.Comment{}, models.SocialMedia{})
+	if err != nil {
+		return
+	}
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
